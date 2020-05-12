81f67785365c83c7dbae89976b55d5d9309a991c
Khoirunnisa' Rizky <khoirunnisa.rizky.n@mail.ugm.ac.id>
2020-05-12 10:29:02 +0700

    Initial Commit

---
+++ PracticeAutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PracticeAutoLayout
+//
+//  Created by khoirunnisa' rizky noor fatimah on 12/05/20.
+//  Copyright © 2020 khoirunnisa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
